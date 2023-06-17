16dc2058fcc0db875a5d5caadca05f202ce752f9
PawelDobrowolsk1 <pdobrowolski001@gmail.com>
2023-06-17 18:08:22 +0200

    Created ToDo class

---
+++ MinimalAPI/ToDo/ToDo.cs
@@ -0,0 +1,8 @@
+﻿namespace MinimalAPI.ToDo;
+
+public class ToDo
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Value { get; set; }
+    public bool isCompleted { get; set; }   
+}
