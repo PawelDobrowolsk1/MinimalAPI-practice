1a37f0f4cbfbebde5a3e6a8934f7f33279f2f363
PawelDobrowolsk1 <pdobrowolski001@gmail.com>
2023-06-18 14:03:58 +0200

    Added validator for ToDo class

---
+++ MinimalAPI/ToDo/ToDoValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace MinimalAPI;
+public class ToDoValidator : AbstractValidator<ToDo>
+{
+    public ToDoValidator()
+    {
+        RuleFor(x => x.Value).NotEmpty()
+            .MinimumLength(5)
+            .WithMessage("Value of a todo must be at least 5 characters");
+    }
+}
